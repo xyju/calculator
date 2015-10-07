2eeaf27cf42d284feaef4de5ec98a20d416ef929
xyju <xyju90@gmail.com>
2015-10-07 16:20:46 -0700

    Initial Commit

---
+++ Calculator/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Calculator
+//
+//  Created by Xiangyu Ju on 15/10/7.
+//  Copyright © 2015年 Xiangyu Ju. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
